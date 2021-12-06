919d545353a6661b41f88e37369a58f721f6141b
VicE0 <vespeche@fi.uba.ar>
2021-12-06 00:59:06 -0300

    git dice que comitee, tonces lo hago (borre 2 lineas en el destructor

--- jugador.cpp
+++ jugador.cpp
@@ -328,10 +328,7 @@ void Jugador::mostrar_objetivos()
 Jugador::~Jugador(){
     delete inventario;
     delete inventario_a_recolectar;
-    delete objetivos;
     delete lista_objetivos;
     inventario = 0;
     inventario_a_recolectar = 0;
-    objetivos = 0;
-    cantidad_objetivos = 0;
 }
